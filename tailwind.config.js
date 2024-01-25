2484e00ba3502ad1db4e37a0abf11918a41b61fa
Bakare Abiola <iamstarcode@gmail.com>
2024-01-25 22:14:14 +0100

    chore: create layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["src/app/**/*.{ts,tsx}", "src/components/**/*.{ts,tsx}"],
   theme: {
     extend: {
-      colors:{
-        'i-primary':"#291507"
-      }
+      colors: {
+        "i-primary": "#291507",
+      },
     },
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@vechaiui/core"),
-  ],
+  plugins: [],
 }
