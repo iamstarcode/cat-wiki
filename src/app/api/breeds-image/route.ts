0979c800dbeeef7a5066812b7074135298eedbc3
StarCode <iamstarcode@gmail.com>
2024-05-12 20:44:05 +0100

    chore: update

--- src/app/api/breeds-image/route.ts
+++ src/app/api/breeds-image/route.ts
@@ -14,22 +14,26 @@ export async function GET(request: Request) {
 
   for (const breedId of breedIds!) {
     //Image URL parsed[0].url
-    const res = await fetch(
-      `https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=${breedId}`,
-      {
-        method: "GET",
-        headers,
-      }
-    )
+    try {
+      const res = await fetch(
+        `https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=${breedId}`,
+        {
+          method: "GET",
+          headers,
+        }
+      )
 
-    const data = await res.text()
-    const parsed = JSON.parse(data)
+      const data = await res.text()
+      const parsed = JSON.parse(data)
 
-    mostSearched.push({
-      imageUrl: parsed[0].url,
-      name: parsed[0].breeds[0].name,
-      id: parsed[0].breeds[0].id,
-    })
+      mostSearched.push({
+        imageUrl: parsed[0].url,
+        name: parsed[0].breeds[0].name,
+        id: parsed[0].breeds[0].id,
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return Response.json({ data: mostSearched })
